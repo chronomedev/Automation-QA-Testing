15ae9d814995a9377471411f986a584e59a088d7
Hansrenee <hansrenee@live.com>
2019-11-27 15:34:34 +0700

    updated code logout test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,19 +5,24 @@ Copyright ChronomeDev 2019 */
 //import com.gargoylesoftware.htmlunit.BrowserVersion;
 //import org.apache.commons.logging.Log;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 //import org.openqa.selenium.support.ui.ExpectedCondition;
 //import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 //import java.util.ArrayList;
+import javax.swing.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 //import java.util.concurrent.TimeUnit; //buat set time out
 
-public class Main{
+public class Main {
 
     // Publik Instance
     public static ChromeDriver driverGogel;
@@ -26,7 +31,7 @@ public class Main{
     public static String laman_dashboard = "https://community.idntimes.com/dashboard";
 
 
-    public static void ChronomeSplash(){
+    public static void ChronomeSplash() {
         System.out.println("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
         System.out.println("  _____ _                                          _____             \n" +
                 " / ____| |                                        |  __ \\            \n" +
@@ -35,8 +40,13 @@ public class Main{
                 "| |____| | | | | | (_) | | | | (_) | | | | | |  __/ |__| |  __/\\ V / \n" +
                 " \\_____|_| |_|_|  \\___/|_| |_|\\___/|_| |_| |_|\\___|_____/ \\___| \\_/  ");
     }
+
+    public static void fungsiJs(WebElement elemenWeb){
+        JavascriptExecutor eksekusi = (JavascriptExecutor)driverGogel;
+        eksekusi.executeScript("arguments[0].click();", elemenWeb);
+    }
     //controller function according test case
-    public static void loginTest(String variabel_name, String variabel_name2, String param1, String param2){
+    public static void loginTest(String variabel_name, String variabel_name2, String param1, String param2) {
         driverGogel.get(laman_web);
         submit_test test2 = new submit_test(driverGogel);
 
@@ -49,19 +59,26 @@ public class Main{
 
     }
 
-    public static void logoutTest(){
+    public static void logoutTest() {
         //driverGogel.get(laman_dashboard);
-        List<WebElement> listElemen =  driverGogel.findElements(By.className("dropdown-submenu-user"));
+        //List<WebElement> listElemen = driverGogel.findElements(By.cssSelector("ul[class='primary-submenu pull-right']"));
+        List<WebElement> listElemen = driverGogel.findElements(By.cssSelector("a[class='header-icon icon-grey dropdown-toggle']"));
 
-        for(int z = 0;z<listElemen.size();z++){
-            System.out.println("print sini....");
-            System.out.println(listElemen.get(z).getAttribute("innerHTML"));
+        JavascriptExecutor jsjalan = (JavascriptExecutor)driverGogel;
+        //listElemen.get(1).click();
+        List<WebElement> menu_user = listElemen.get(1).findElements(By.tagName("li"));
+        fungsiJs(listElemen.get(0));
+        List<WebElement> tagLink = driverGogel.findElements(By.tagName("a"));
+        for(int z= 0 ;z<tagLink.size();z++){
+            if(tagLink.get(z).getAttribute("href").equals("https://community.idntimes.com/logout")){
+                tagLink.get(z).click();
+                break;
+            }
         }
-        System.out.println(listElemen);
-
-
-
     }
+
+
+
 
 
     public static void editorTest(){
@@ -134,10 +151,15 @@ public class Main{
             System.out.println(System.getProperty("user.dir"));
             //Sesuaikan dengan browser mau yang dipake yang mana setiap engine browser itu berbeda
             System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+ "\\chromedriver.exe");
-            driverGogel = new ChromeDriver();
+            //buat maximize
+            ChromeOptions opsiArgumen = new ChromeOptions();
+            opsiArgumen.addArguments("--start-maximized");
+            driverGogel = new ChromeDriver(opsiArgumen);
+
             loginTest("email", "password", "destianputra@gmail.com", "testingimplementasi");
+            Thread.sleep(4000);
             logoutTest();
-            Thread.sleep(5000);
+            //Thread.sleep(4000);
 
         } catch (Exception e){
             System.out.println("ERROR WAZWUZWUZ:::::");
